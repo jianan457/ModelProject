37dc7f75750449147558a78800250326c9113a83
tracy <277945060@qq.com>
2015-11-24 09:05:27 +0800

    添加读取数据方法

---
+++ CompanyModelProject.Model/NewsWebModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace CompanyModelProject.Model
+{
+    public class NewsWebModel
+    {
+        public int ID { get; set; }
+        public int ColumnId { get; set; }
+        public string HtmlUrl { get; set; }
+        public string Title { get; set; } 
+        public int orders { get; set; } 
+        public DateTime CreateTime { get; set; }
+        public string BriefMain { get; set; }
+        /// <summary>
+        /// 是否可用  0:否 1 :是
+        /// </summary>
+        public bool isDel { get; set; }
+        /// <summary>
+        /// 是否首页推荐
+        /// </summary>
+        public bool IsClomnrecommond { get; set; }
+        public bool IsIndexRecommond { get; set; } 
+    }
+}
